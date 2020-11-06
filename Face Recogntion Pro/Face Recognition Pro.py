b32d76db16e47c4eb45e4e5cc5a2dda5019873de
Palash Ahmed <49250151+palahsu@users.noreply.github.com>
2020-11-06 11:59:34 +0600

    Add files via upload

---
+++ Face Recogntion Pro/Face Recognition Pro.py
@@ -0,0 +1,29 @@
+import cv2 as cv
+
+import numpy as np
+
+# Get user supplied values
+# Create the haar cascade
+
+face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+# Read the image
+image = cv.imread('facere.jpg')
+gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+
+# Detect faces in the image
+faces = face_cascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(30, 30),
+    #flags = cv.cv.CV_HAAR_SCALE_IMAGE
+)
+
+print ("Found {0} faces!" .format(len(faces)))
+
+# Draw a rectangle around the faces
+for (x, y, w, h) in faces:
+    cv.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+cv.imshow("Faces found", image)
+cv.waitKey(0)    
